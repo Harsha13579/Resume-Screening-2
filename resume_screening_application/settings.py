4e48779bc7be8539496d200ff8140513ac8eac94
Jaswanth Challa <162020390+jaswanthchalla@users.noreply.github.com>
2025-04-12 09:32:20 +0530

    Update settings.py

--- resume_screening_application/settings.py
+++ resume_screening_application/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7y0ix(943%=o2f!0vjcv#$v#u6&46-8c#zh*yb@_d_z+=dg*9x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
